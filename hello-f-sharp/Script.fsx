177df1b7bf6c5b43fdc670986ad15407d764d456
ranjeet <ranjeet.floyd@gmail.com>
2020-01-13 22:39:29 +0530

    Initial commit

---
+++ hello-f-sharp/Script.fsx
@@ -0,0 +1,6 @@
+﻿let distance x y = x - y |> abs
+let distanceFrom5 = distance 5
+distanceFrom5 -5
+(distance 5) 2
+
+
---
+++ hello-f-sharp/function_composition.fsx
@@ -0,0 +1,23 @@
+﻿// Functional Composition | 
+
+let minus1 x = x - 1
+let times2 = (*) 2
+
+minus1 9
+times2 8
+
+// same
+times2 (minus1 9)
+
+// backward composition
+let minus1ThenTimes2 = times2 << minus1
+minus1ThenTimes2 9 // 16
+
+let times2ThenMinus1 = times2 >> minus1
+
+times2ThenMinus1 9 // 17
+
+
+
+
+
